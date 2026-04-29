e0ce745ce4ad8df9e3593c806250029d9f5d26c8
Nit <mehmetgoker1888@gmail.com>
2026-04-29 12:57:51 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T09:57:10.244Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-04-29T09:57:49.095Z
+Commit: Fix CSS styling bug on mobile devices
