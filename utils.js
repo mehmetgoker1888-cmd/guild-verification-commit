303b45aeeb8eebebd519332a7b95859ec23ba9bc
Nit <mehmetgoker1888@gmail.com>
2026-04-29 13:01:56 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T10:01:31.836Z
-Commit: Add export functionality for reports
+Timestamp: 2026-04-29T10:01:54.246Z
+Commit: Fix memory leak in event handlers
