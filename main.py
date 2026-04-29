ab14cb88d876147a4d2bd9121d40f2395342b997
Nit <mehmetgoker1888@gmail.com>
2026-04-29 13:03:17 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T10:03:03.076Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-04-29T10:03:15.382Z
+Commit: Introduce real-time notifications
