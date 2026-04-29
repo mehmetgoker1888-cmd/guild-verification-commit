c92df3011a9f925fea7e544b3cb6bc83d9e8af64
Nit <mehmetgoker1888@gmail.com>
2026-04-29 12:58:49 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T09:58:11.196Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-04-29T09:58:47.080Z
+Commit: Resolve null pointer exception in user service
