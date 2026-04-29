2388124c90d96e0d4b977d845c6f6221498e6a54
Nit <mehmetgoker1888@gmail.com>
2026-04-29 13:01:13 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T10:00:38.210Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-04-29T10:01:11.087Z
+Commit: Fix broken links in documentation
