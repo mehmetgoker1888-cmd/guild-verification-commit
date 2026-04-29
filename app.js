8f802984133b6f8c0348fade27242a6b00233380
Nit <mehmetgoker1888@gmail.com>
2026-04-29 13:04:10 +0300

    Patch API endpoint error handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T10:04:00.976Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-04-29T10:04:07.777Z
+Commit: Patch API endpoint error handling
