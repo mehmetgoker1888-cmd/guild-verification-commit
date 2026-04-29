113f6295fa5c8a6f563ae9bdb315e76524542ef7
Nit <mehmetgoker1888@gmail.com>
2026-04-29 13:03:43 +0300

    Add export functionality for reports

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T10:03:13.052Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-04-29T10:03:40.658Z
+Commit: Add export functionality for reports
