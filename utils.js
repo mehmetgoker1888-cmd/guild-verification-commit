a1d45a689e9e9116f5e9dfb4fafdec419979412e
Nit <mehmetgoker1888@gmail.com>
2026-04-29 12:59:15 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T09:58:35.475Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-04-29T09:59:12.969Z
+Commit: Add export functionality for reports
