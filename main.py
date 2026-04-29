ebad5230b4babd51ed521b9a48b0a9edd35bd8da
Nit <mehmetgoker1888@gmail.com>
2026-04-29 13:01:00 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T10:00:53.743Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-04-29T10:00:57.883Z
+Commit: Fix broken links in documentation
