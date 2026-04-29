babcfc5d166768ab307c9f4f4839ae9d611f0501
Nit <mehmetgoker1888@gmail.com>
2026-04-29 12:59:17 +0300

    Add multi-language support

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T09:58:56.074Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-04-29T09:59:15.110Z
+Commit: Add multi-language support
