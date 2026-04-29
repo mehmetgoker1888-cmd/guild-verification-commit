2defbc3002cbb6c2fc64b1bd6321772c5f021e2c
Nit <mehmetgoker1888@gmail.com>
2026-04-29 12:59:43 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T09:59:12.969Z
-Commit: Add export functionality for reports
+Timestamp: 2026-04-29T09:59:41.181Z
+Commit: Fix race condition in async operations
