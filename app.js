6b1cb4ea30789a62b828bce6ee3cade07af88e89
Nit <mehmetgoker1888@gmail.com>
2026-04-29 13:00:15 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T10:00:11.223Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-04-29T10:00:13.324Z
+Commit: Fix broken links in documentation
