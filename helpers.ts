50b0eb9edb25ca9cbdb566919c7c07039e693a44
Nit <mehmetgoker1888@gmail.com>
2026-04-29 12:56:34 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T09:56:29.811Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-04-29T09:56:31.930Z
+Commit: Fix race condition in async operations
