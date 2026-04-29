12c21c4e7c744cf4d50c0e3c44b2c985a3e6d01d
Nit <mehmetgoker1888@gmail.com>
2026-04-29 12:57:53 +0300

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-29T09:57:36.832Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-04-29T09:57:51.195Z
+Commit: Introduce real-time notifications
